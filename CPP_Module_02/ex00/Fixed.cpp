e8e022caa6c273441d12374b77d54366dfdd3098
Lurik13 <lucasribette.pro@gmail.com>
2024-04-09 20:30:44 +0200

    Update Fixed.cpp

--- CPP_Module_02/ex00/Fixed.cpp
+++ CPP_Module_02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: lribette <lribette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 18:49:33 by lribette          #+#    #+#             */
-/*   Updated: 2024/04/09 19:49:31 by lribette         ###   ########.fr       */
+/*   Updated: 2024/04/09 20:24:14 by lribette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 Fixed::Fixed()
 {
-	std::cout << "\033[3mDefault constructor called\033[0m" << std::endl;
+	std::cout << "Default constructor called" << std::endl;
 	_value = 0;
 }
 
@@ -26,12 +26,24 @@ Fixed::Fixed(Fixed &copy)
 
 Fixed &Fixed::operator=(const Fixed &source)
 {
-	std::cout << "\033[1mCopy assignment operator called\033[0m" << std::endl;
+	std::cout << "\033[9mCopy assignment operator called\033[0m" << std::endl;
 	_value = source._value;
-	return *this;
+	return (*this);
 }
 
 Fixed::~Fixed()
 {
-	std::cout << "\033[1mDestructor called\033[0m" << std::endl;
+	std::cout << "\033[7mDestructor called\033[0m" << std::endl;
+}
+
+int Fixed::getRawBits( void ) const
+{
+	std::cout << "\033[3mgetRawBits member function called\033[0m" << std::endl;
+	return (1);
+}
+
+void Fixed::setRawBits( int const raw )
+{
+	std::cout << "\033[1msetRawBits member function called\033[0m" << std::endl;
+	_value = raw;
 }
