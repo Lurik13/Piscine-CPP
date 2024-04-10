a13b9b12bb4162fe475c84dbee8637371b0ed810
Lurik13 <lucasribette.pro@gmail.com>
2024-04-10 09:24:17 +0200

    ex00 finished

--- CPP_Module_02/ex00/Fixed.cpp
+++ CPP_Module_02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: lribette <lribette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 18:49:33 by lribette          #+#    #+#             */
-/*   Updated: 2024/04/09 20:24:14 by lribette         ###   ########.fr       */
+/*   Updated: 2024/04/10 09:23:43 by lribette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 Fixed::Fixed()
 {
 	std::cout << "Default constructor called" << std::endl;
-	_value = 0;
+	this->_value = 0;
 }
 
 Fixed::Fixed(Fixed &copy)
 {
 	std::cout << "\033[4mCopy constructor called\033[0m" << std::endl;
-	_value = copy._value;
+	this->_value = copy._value;
 }
 
 Fixed &Fixed::operator=(const Fixed &source)
 {
 	std::cout << "\033[9mCopy assignment operator called\033[0m" << std::endl;
-	_value = source._value;
+	this->_value = source._value;
 	return (*this);
 }
 
@@ -39,11 +39,11 @@ Fixed::~Fixed()
 int Fixed::getRawBits( void ) const
 {
 	std::cout << "\033[3mgetRawBits member function called\033[0m" << std::endl;
-	return (1);
+	return (this->_value);
 }
 
 void Fixed::setRawBits( int const raw )
 {
 	std::cout << "\033[1msetRawBits member function called\033[0m" << std::endl;
-	_value = raw;
+	this->_value = raw;
 }
--- CPP_Module_02/ex00/main.cpp
+++ CPP_Module_02/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lribette <lribette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 18:48:44 by lribette          #+#    #+#             */
-/*   Updated: 2024/04/09 19:25:10 by lribette         ###   ########.fr       */
+/*   Updated: 2024/04/10 09:22:09 by lribette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,17 @@ int main( void )
 	
 	c = b;
 	
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
+	std::cout << "a._value = " << a.getRawBits() << std::endl;
+	std::cout << "b._value = " << b.getRawBits() << std::endl;
+	std::cout << "c._value = " << c.getRawBits() << std::endl;
+	a.setRawBits(42);
+	std::cout << "a._value = " << a.getRawBits() << std::endl;
+	std::cout << "b._value = " << b.getRawBits() << std::endl;
+	std::cout << "c._value = " << c.getRawBits() << std::endl;
+	b.setRawBits(63);
+	std::cout << "a._value = " << a.getRawBits() << std::endl;
+	std::cout << "b._value = " << b.getRawBits() << std::endl;
+	std::cout << "c._value = " << c.getRawBits() << std::endl;
 	
 	return 0;
 }
