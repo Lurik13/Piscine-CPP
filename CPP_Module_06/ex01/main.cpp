6c0e65a7b4b3459cc1099b08d8ea5fce328beaee
Lucas Ribette <lucasribette.pro@gmail.com>
2024-08-15 20:20:06 +0200

    CPP06/ex02

--- CPP_Module_06/ex01/main.cpp
+++ CPP_Module_06/ex01/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 	
 	if (argc != 2 || static_cast<std::string>(argv[1]).find_first_not_of("0123456789") != std::string::npos)
 	{
-		std::cout << "\e[38;2;170;0;0;1mWrong params." << std::endl;
+		std::cout << "\e[38;2;170;0;0;1mWrong params.\e[0m" << std::endl;
 		return (EXIT_FAILURE);
 	}
 	Data data;
---
+++ CPP_Module_06/ex02/Classes.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Base.hpp"
+
+class A: public Base
+{
+};
+
+class B: public Base
+{
+};
+
+class C: public Base
+{
+};
