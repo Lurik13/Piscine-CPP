ac31aed839eadacf42b04283382bfecebb628f82
Lucas Ribette <lucasribette.pro@gmail.com>
2024-10-04 14:16:13 +0200

    Easier to read

--- CPP_Module_08/ex02/main.cpp
+++ CPP_Module_08/ex02/main.cpp
@@ -1,9 +1,11 @@
 #include "MutantStack.hpp"
 #include "MutantStack.tpp"
 
-void responsiveness(int spaces)
+int responsive_spaces;
+
+void responsiveness()
 {
-	for (int i = 0; i < spaces; ++i)
+	for (int i = 0; i < responsive_spaces; ++i)
 		std::cout << " ";
 }
 
@@ -14,26 +16,25 @@ int main(int argc, char **argv)
 		std::cout << ERROR "I need \e[7mtput cols" RESET ERROR "!" << std::endl;
 		return (EXIT_FAILURE);
 	}
-	int responsive_spaces = (atoi(argv[1]) - 63) / 2;
-	
+	responsive_spaces = (atoi(argv[1]) - 63) / 2;
 	{
-		responsiveness(responsive_spaces);
+		responsiveness();
 		std::cout << TOP;
-		responsiveness(responsive_spaces);
+		responsiveness();
 		std::cout << LEFT "                " PURPLE "Tests with our MutantStack:" RESET
 		<< "                  " RIGHT << std::endl;
-		responsiveness(responsive_spaces);
+		responsiveness();
 		std::cout << LEFT EMPTY_LINE RIGHT << std::endl;
 
 		MutantStack<int> mstack;
 		mstack.push(5);
 		mstack.push(17);
-		responsiveness(responsive_spaces);
+		responsiveness();
 		std::cout << LEFT "Top value after pushing 5 and 17: " RESET
 		<< mstack.top() << "                         " RIGHT << std::endl;
 
 		mstack.pop();
-		responsiveness(responsive_spaces);
+		responsiveness();
 		std::cout << LEFT "Stack size after removing the top value: " RESET
 		<< mstack.size() << "                   " RIGHT << std::endl;
 		
@@ -41,12 +42,12 @@ int main(int argc, char **argv)
 		mstack.push(5);
 		mstack.push(737);
 		mstack.push(0);
-		responsiveness(responsive_spaces);
+		responsiveness();
 		std::cout << LEFT EMPTY_LINE RIGHT << std::endl;
-		responsiveness(responsive_spaces);
+		responsiveness();
 		std::cout << LEFT "We stack 4 other values.                                     " << RIGHT << std::endl;
 
-		responsiveness(responsive_spaces);
+		responsiveness();
 		std::cout << LEFT "Displaying the stack using our iterator: " RESET;
 		MutantStack<int>::iterator first = mstack.begin();
 		MutantStack<int>::iterator last = mstack.end();
@@ -60,25 +61,25 @@ int main(int argc, char **argv)
 				std::cout << "; ";
 		}
 		std::cout << "     " RIGHT << std::endl;
-		responsiveness(responsive_spaces);
+		responsiveness();
 		std::cout << MIDDLE;
 	}
 	{
-		responsiveness(responsive_spaces);
+		responsiveness();
 		std::cout << LEFT "                     " PURPLE "Tests with a list:" RESET
 		<< "                      " RIGHT << std::endl;
-		responsiveness(responsive_spaces);
+		responsiveness();
 		std::cout << LEFT EMPTY_LINE RIGHT << std::endl;
 
 		std::list<int> mlist;
 		mlist.push_back(5);
 		mlist.push_back(17);
-		responsiveness(responsive_spaces);
+		responsiveness();
 		std::cout << LEFT "Top value after pushing 5 and 17: " RESET
 		<< mlist.back() << "                         " RIGHT << std::endl;
 
 		mlist.pop_back();
-		responsiveness(responsive_spaces);
+		responsiveness();
 		std::cout << LEFT "Stack size after removing the top value: " RESET
 		<< mlist.size() << "                   " RIGHT << std::endl;
 		
@@ -86,12 +87,12 @@ int main(int argc, char **argv)
 		mlist.push_back(5);
 		mlist.push_back(737);
 		mlist.push_back(0);
-		responsiveness(responsive_spaces);
+		responsiveness();
 		std::cout << LEFT EMPTY_LINE RIGHT << std::endl;
-		responsiveness(responsive_spaces);
+		responsiveness();
 		std::cout << LEFT "We stack 4 other values.                                     " << RIGHT << std::endl;
 
-		responsiveness(responsive_spaces);
+		responsiveness();
 		std::cout << LEFT "Displaying the stack using the list iterator: " RESET;
 		int size = mlist.size();
 		while (size)
@@ -103,7 +104,7 @@ int main(int argc, char **argv)
 				std::cout << "; ";
 		}
 		std::cout << RIGHT << std::endl;
-		responsiveness(responsive_spaces);
+		responsiveness();
 		std::cout << BOTTOM;
 	}
 	return 0;
